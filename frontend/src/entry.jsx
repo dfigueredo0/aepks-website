2f3eb07b866e8837afd9a112dc1442e51edaf900
Dylan Figueredo <d.figueredo2104@gmail.com>
2025-08-15 01:00:12 -0500

    Start of Dither Background

--- frontend/src/entry.jsx
+++ frontend/src/entry.jsx
@@ -2,6 +2,23 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import RecruitmentTrail from "./RecruitmentTrail.jsx";
 
+export function App() {
+  return ( 
+  <div style={{ width: '100%', height: '600px', position: 'relative' }}>
+    <Dither
+      waveColor={[0.5, 0.5, 0.5]}
+      disableAnimation={false}
+      enableMouseInteraction={true}
+      mouseRadius={0.3}
+      colorNum={4}
+      waveAmplitude={0.3}
+      waveFrequency={3}
+      waveSpeed={0.05}
+    />
+  </div>
+  )
+}
+
 export function mount(el, props = {}) {
   const root = createRoot(el);
   root.render(<RecruitmentTrail {...props} />);
